c957a330d1383957ea911ff9e4361f375a565626
Alhexx <guimaraes.aco@gmail.com>
2021-06-11 22:35:20 +0000

    atualizacao

--- soma_vizinhos/src/main.cpp
+++ soma_vizinhos/src/main.cpp
@@ -4,13 +4,46 @@
  * @data June, 6th 2021
  */
 #include <iostream>
+/*
 using std::cout;
 using std::cin;
 using std::endl;
+*/
+using namespace std;
 
 int main( void )
 {
-    
-
-    return 0;
+  int a, b;
+  bool rodando = true;
+  while(cin>>a>>b)
+  {
+    int soma=0;
+      
+    if(b>0)
+    {
+      for(int i=a; i<a+b;i++)
+      {
+        
+        soma+=i;
+        //cout<<i<<" ";
+      }
+      cout<<soma<<endl;
+    }
+    else if(b<0)
+    {
+      for(int i=0; i<-b;i++)
+      {
+        soma+=a;
+        a-=1;
+        //cout<<a<<" ";
+      }
+      //cout<<endl;
+      cout<<soma<<endl;
+    }
+    else
+    {
+      cout<<a<<endl;
+    }
+}
+  return 0;
 }
