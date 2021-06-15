f406fcaacd3708608a4d096290a1db95f4dfa03d
Alhexx <guimaraes.aco@gmail.com>
2021-06-15 15:53:46 +0000

    atualização fibonacci

--- fibonacci/src/function.cpp
+++ fibonacci/src/function.cpp
@@ -2,8 +2,26 @@
 
 std::vector<unsigned int> fib_below_n( unsigned int n )
 {
-    // TODO: adicione o seu código aqui.
+  if (n <= 1)
+  {
+    vector<unsigned int> v1;
+    return v1;
+  }
+    
+  int n1=1, n2=1, aux=0;
+  vector<unsigned int> v={1, 1};
 
-    // TODO: Isto é apenas um STUB. Troque o retorno pelo que você julgar correto.
-    return std::vector<unsigned int>{};
+
+  while(aux<n)
+  {
+    aux=n1+n2;
+    n1=n2;
+    n2=aux;
+    if (aux >= n)
+      break;
+    v.push_back(aux);
+  }
+
+    
+  return v;
 }
